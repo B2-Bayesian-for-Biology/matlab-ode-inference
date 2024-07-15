8382bdb178ba94ffaf5c3f98f10ad3e2c4e2bab3
RaunakDey <deyraunak111@gmail.com>
2024-07-15 09:02:42 -0400

    run1 dram
    log transformed LL/logNormal priors

--- scripts/bayesian_fit_2.m
+++ scripts/bayesian_fit_2.m
@@ -75,3 +75,60 @@ filename = "logtransformed_" + string(flags.logtransformed) + "_logNormal_" + st
 path = "./../results/";
 save(path + filename);
 
+
+%% plotting and saving figure
+
+addpath('./../tools/');
+
+% Time span
+tspan_finer = 0:0.1:13.5;
+
+%without the non-negative this problem will lead to an infeasible solution.
+opts = odeset('RelTol',1e-2,'AbsTol',1e-5,'NonNegative',1);
+
+transparency = 0.1;
+linewidth = 2;
+
+figure(2)
+plot(time, cells, 'ko','MarkerSize',8,'LineStyle','none','MarkerFaceColor','k','MarkerEdgeColor','k');
+hold on;
+set(gca,'YScale','log');
+
+
+for i = 2500:10:5000
+
+i
+
+theta = chain(i,:);
+% Parameters
+mu_max = theta(1);
+Ks = theta(2);
+
+if flags.logNormal == 1
+Qn = 10.^theta(3);
+N0 = 10.^theta(4);
+else
+Qn = theta(3);
+N0 = theta(4);  
+end
+
+
+% Initial conditions
+y0 = [N0, cells(1)];
+
+
+% Solve the ODE
+[t, y] = ode45(@(t, y) growth_ode(t, y, mu_max, Ks, Qn), tspan_finer, y0,opts);
+
+
+figure(2)
+patchline(t,y(:,2),'edgecolor',[0 0 1],'edgealpha',transparency,'LineWidth',linewidth)
+
+end
+
+
+xlabel('Time');
+ylabel('Cells');
+
+set(gca,'Fontsize',20);
+set(gca,'fontname','times');
